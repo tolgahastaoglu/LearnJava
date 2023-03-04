f03b139e3b2c2c7bbb7bc657a502353da4be83d2
tolga <tolga@tolga-550P5C-550P7C>
2023-03-05 00:53:21 +0300

    ProductDao

--- oopWithNLayeredApp/src/oopWithNLayeredApp/business/ProductManager.java
+++ oopWithNLayeredApp/src/oopWithNLayeredApp/business/ProductManager.java
@@ -1,5 +1,17 @@
 package oopWithNLayeredApp.business;
 
+import oopWithNLayeredApp.dataAccess.JdbcProductDao;
+import oopWithNLayeredApp.entities.Product;
+
 public class ProductManager {
 
+	public void add(Product product) throws Exception {
+		//iş kuralları
+		if (product.getUnitPrice()<10) {
+			throw new Exception("Ürün fiyatı 10'dan küçük olamaz");
+		}
+		JdbcProductDao productDao=new JdbcProductDao();
+		productDao.add(product);
+	
+	}	
 }
